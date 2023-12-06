4c03bfde9199fe75d271ad737d018c9e944791e8
jairohrdev <j.gcarballa.dev@gmail.com>
2023-12-06 17:53:12 +0100

    Navbar Ant

--- src/components/RouteCard.jsx
+++ src/components/RouteCard.jsx
@@ -1,31 +1,28 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import RouteMap from './RouteMap'
-import { Card } from 'antd';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import RouteMap from "./RouteMap";
+import { Card } from "antd";
 
 const { Meta } = Card;
 
 function RouteCard(props) {
-    const { destiny, origin, user, _id } = props.data;
-  
-    
+  const { destiny, origin, user, _id } = props.data;
 
   return (
-    
-    <div id='route-Card'>
-       <Link to={`/routeDetails/${_id}`} >
-        <div className='user-info'>
-            <h3>{user.username}</h3>
-            <div className='img-container-list'>
-            <img src={user.userPicture} alt="motoimg"/>
-            </div>
+    <div id="route-Card" style={{ backgroundColor: "white" }}>
+      <Link to={`/routeDetails/${_id}`}>
+        <div className="user-info">
+          <h3>{user.username}</h3>
+          <div className="img-container-list">
+            <img src={user.userPicture} alt="motoimg" />
+          </div>
         </div>
-        <div className='map-container'>
-            <RouteMap origin={origin} destiny={destiny}/>
+        <div className="map-container">
+          <RouteMap origin={origin} destiny={destiny} />
         </div>
-        </Link>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default RouteCard
+export default RouteCard;
